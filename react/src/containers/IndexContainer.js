07c7b3e0f61b825ca0e085acacee75f81c364e63
john mauran <johnmauran@johns-MacBook-Air.local>
2017-05-12 13:28:09 -0400

    Add more instructions

--- react/src/containers/IndexContainer.js
+++ react/src/containers/IndexContainer.js
@@ -24,7 +24,8 @@ class IndexContainer extends React.Component {
       <br />
       <br />
       <br />
-      <h3> To record a tune, sign up or sign in. Hold ctrl to go down an octave. </h3>
+      <h3> To record a tune, sign up or sign in.  </h3>
+      <h3> Use your mouse or keyboard to play. Using the keyboard, hold ctrl to go down an octave.  </h3>
       </div>
       </div>
     )
